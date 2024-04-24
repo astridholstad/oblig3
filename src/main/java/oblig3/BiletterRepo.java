c79f0078c60228f452fa53aa757edba2b5bd5e4e
astridholstad <astridholstad21@gmail.com>
2024-04-24 11:12:15 +0200

    oppatering2

---
+++ src/main/java/oblig3/BiletterRepo.java
@@ -0,0 +1,17 @@
+package oblig3;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import java.util.Collections;
+import java.util.List;
+/*
+@Repository
+public class BiletterRepo {
+
+    @Autowired
+    private JdbcTemplate db;
+
+}
+
+
+
+*/
